b28228cae7e0e9f874a4d47a1a8caa4f3c0a5846
TheRealArccc <163712315+TheRealArccc@users.noreply.github.com>
2025-11-24 21:58:36 +0800

    Added basic function support

--- src/runtime/interpreter.py
+++ src/runtime/interpreter.py
@@ -29,6 +29,60 @@ class Interpreter:
 
         return result
     
+    def visit_ReturnNode(self, node):
+        raise ReturnSignal(self.visit(node.value))
+    
+    def visit_ParameterNode(self, node):
+        print(node.params.value)
+        return node.params.value
+
+    def visit_FunctionDefNode(self, node):
+        function = FunctionValue(node.name, node.params, node.body, self.env)
+        self.env.declare(node.name, function)
+
+    def visit_FunctionCallNode(self, node):
+        func = self.env.get(node.name.value)
+
+        call_env = Environment(func.env)
+        call_env.enter_scope()
+
+        old_env = self.env
+        self.env = call_env
+        for param, arg in zip(func.params, node.args):
+            call_env.declare(self.visit(param), self.visit(arg))
+        
+        try:
+            for stmt in func.body:
+                self.visit(stmt)
+        except ReturnSignal as r:
+            result = r.value
+            return r
+
+        call_env.exit_scope()
+        self.env = old_env
+
+        print(func)
+        print(self.env.scopes)
+    
+    def visit_WhileNode(self, node):
+        self.env.enter_scope()
+        while self.visit(node.condition):
+            for stmt in node.body:
+                self.visit(stmt)
+        self.env.exit_scope()
+        print(self.env.scopes)
+    
+    def visit_ForNode(self, node):
+        self.env.enter_scope()
+        self.visit(node.init)
+        while self.visit(node.condition):
+            for stmt in node.body:
+                self.visit(stmt)
+            
+            self.visit(node.increment)
+        self.env.exit_scope()
+        print(self.env.scopes)
+
     def visit_IfNode(self, node):
         condition = self.visit(node.condition)
         if condition:
@@ -45,12 +99,12 @@ class Interpreter:
     def visit_AssignNode(self, node):
         value = self.visit(node.value)
         identifier = node.identifier
-        self.env.assign_var(identifier, value)
+        self.env.assign(identifier, value)
         print("SCOPE: ", self.env.scopes)
 
     def visit_VarDeclNode(self, node):
         value = self.visit(node.value)
-        self.env.declare_var(node.identifier, value)
+        self.env.declare(node.identifier, value)
         print("SCOPE: ", self.env.scopes)
 
     def visit_UnaryOpNode(self, node):
@@ -71,12 +125,16 @@ class Interpreter:
         def eval_values(left, op, right):
             left = getattr(left, 'value', left)
             right = getattr(right, 'value', right)
+            # Nothing value
+            if op in ('+', '-', '*', '/') and None in (left, right):
+                print(left)
+                raise TypeError(f"Cannot perform operations with 'Nothing' type")
             if op == '+': return (left+right)
-            elif op == '-':
-                if right != 0: return (left-right) 
-                else: raise ZeroDivisionError("Cannot divide by zero")
+            elif op == '-': return (left-right)
             elif op == '*': return (left*right)
-            elif op == '/': return (left/right)
+            elif op == '/':
+                if right != 0: return (left/right) 
+                else: raise ZeroDivisionError("Cannot divide by zero")
             elif op == '==': return bool(left==right)
             elif op == '>': return bool(left>right)
             elif op == '<': return bool(left<right)
@@ -84,13 +142,13 @@ class Interpreter:
             elif op == '>=': return bool(left>=right)
             elif op == '!=': return bool(left!=right)
             elif op == '&&':
-                if not left:
+                if left == False:
                     return False
-                return bool(self.visit(right))
+                return bool(right)
             elif op == '||':
-                if left:
-                    return True
-                return bool(self.visit(right))
+                if right == True:
+                    return right
+                return bool(left)
 
         left = self.visit(node.left)
         right = self.visit(node.right)
@@ -123,13 +181,26 @@ class Interpreter:
         
     def visit_VariableNode(self, node):
         identifier = node.identifier
-        value = self.env.get_var(identifier)
+        value = self.env.get(identifier)
         return value
+    
+    def visit_PostfixOpNode(self, node):
+        identifier = node.identifier
+        value = self.env.get(identifier)
+        if node.op.type == TokenType.PLUSPLUS:
+            old_value = value
+            value.value += 1
+            self.env.assign(identifier, value)
+        if node.op.type == TokenType.MINUSMINUS:
+            old_value = value
+            value.value -= 1
+            self.env.assign(identifier, value)
+        return old_value
         
     def visit_NothingLiteralNode(self, node):
         token = node.literal
         if token == None:
-            return NothingLiteralNode(token)
+            return NothingValue(token)
     
     def visit_LiteralNode(self, node):
         token = node.literal
